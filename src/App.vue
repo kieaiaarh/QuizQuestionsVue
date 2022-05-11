b94dda4c46e183154ab1cf9b68c2afa57ef6400e
kieaiaarh <kieaiaarh@gmail.com>
2022-05-11 23:51:21 +0900

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <QuizQuestions />
+</template>
+
+<script>
+import QuizQuestions from "./components/QuizQuestions.vue";
+export default {
+  name: "App",
+  components: {
+    QuizQuestions,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
